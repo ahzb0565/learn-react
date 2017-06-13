9be36f792ace3d133c7d1d3020f2f92b14e59209
Bob <ahzb0565@sina.com>
2017-06-13 15:26:43 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Learn React
+###### For Bob to study react
+
+### Usage:
+1. git clone
+2. npm install
+3. npm start
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack');
+module.exports = {
+  entry: './index.jsx',
+  output: {
+    path: __dirname + './dist',  //abs path
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+};
